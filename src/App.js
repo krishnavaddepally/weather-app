9eea34ab13b9d2d920708d23a78d7642e63c2322
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-19 10:18:24 -0500

    Created new component Titles and rendered information to the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Titles from "./components/Titles"
 
 class App extends Component {
   render() {
     return(
       <div>
-        <h1>Welcome to react app!</h1>
+        <Titles/>
       </div>
     );
   }
