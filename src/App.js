a8c12187a8ab72d4dbfd695c517b01b52abcf159
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-19 10:31:12 -0500

    Added Form Container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Titles from "./components/Titles"
+import Form ffrom "./components/Form"
 
 class App extends Component {
   render() {
     return(
       <div>
         <Titles/>
+        <Form/>
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react"
+class Form extendes React.Component{
+  render(){
+    return(
+      <div>Form Component</div>
+    );
+  }
+}
+
+export default Form;
