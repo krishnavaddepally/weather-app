84f8bb0509fefd6d1e0cd1e14b1b60b1a0106257
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-20 11:22:16 -0500

    Added an API and dynamically changed city and country values

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Titles from "./components/Titles"
 import Form from "./components/Form"
 import Weather from "./components/Weather"
 const API_KEY = "cd128563b8d249a2bcd1cabdafd6a782";
 
-class App extends Component {
-  getWeather= async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+class App extends React.Component {
+  
+  getWeather= async(event) => {
+    event.preventDefault();
+    const city= event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
   }
   render() {
     return(
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather={this.getWeather}/>
         <Weather/>
       </div>
     );
