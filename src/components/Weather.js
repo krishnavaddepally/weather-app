fbff17ecf0e99edc4fbad5c59c5ea6d8be283745
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-20 12:47:41 -0500

    Added error messages

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,40 @@ import React from "react"
 class Weather extends React.Component{
   render(){
     return(
-      <div>Weather Component</div>
+      <div>
+          {this.props.city && this.props.country &&
+            <div>
+              <label>Location:</label>
+              {this.props.city},{this.props.country}
+            </div>
+          }
+          {this.props.temperature &&
+            <div>
+              <label>Temperature:</label>
+              {this.props.temperature}
+            </div>
+          }
+          {
+            this.props.humidity &&
+            <div>
+              <label>Humidity:</label>
+              {this.props.humidity}
+            </div>
+          }
+          {
+            this.props.description &&
+            <div>
+              <label>Conditions: </label>
+              {this.props.description}
+            </div>
+          }
+          {
+            this.props.error &&
+            <div>
+              {this.props.error} 
+            </div>
+          }
+      </div>
     );
   }
 }
